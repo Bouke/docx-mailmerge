c6d8a61565269f3d0b9d41539ee0873ae273998d
Bouke Haarsma <bouke@webatoom.nl>
2013-05-22 20:16:47 +0200

    Added test for Mac Word 2011 documents

---
+++ tests/test_macword2011.py
@@ -0,0 +1,19 @@
+import unittest
+import tempfile
+from mailmerge import MailMerge
+
+
+class MacWord2011Test(unittest.TestCase):
+    def test(self):
+        document = MailMerge('tests/test_macword2011.docx')
+        self.assertEquals(document.get_merge_fields(),
+                          set(['first_name', 'last_name', 'country', 'state',
+                               'postal_code', 'date', 'address_line', 'city']))
+
+        document.merge(first_name='Bouke', last_name='Haarsma',
+                       country='The Netherlands', state=None,
+                       postal_code='9723 ZA', city='Groningen',
+                       address_line='Helperpark 278d', date='May 22nd, 2013')
+
+        with tempfile.NamedTemporaryFile() as outfile:
+            document.write(outfile.name)
