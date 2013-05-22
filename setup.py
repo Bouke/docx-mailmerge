f13c2b49e3c486b2621e7f792e52c8719af96d28
Bouke Haarsma <bouke@webatoom.nl>
2013-05-22 20:37:03 +0200

    Up'ed version

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup, find_packages
 
-version = '0.0.2'
+version = '0.1.0'
 
 setup(name='docx-mailmerge',
       version=version,
       description='Performs a Mail Merge on docx (Microsoft Office Word) files',
       long_description=open('README.rst').read(),
       classifiers=[
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.3',
           'Topic :: Text Processing',
       ],
       author='Bouke Haarsma',
