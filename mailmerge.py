e8150f0acbc9625088b976b2b2db235a419b12b7
tom1200 <33700862+tom1200@users.noreply.github.com>
2018-01-29 21:50:17 +0100

    Customisable separator for merging templates

--- mailmerge.py
+++ mailmerge.py
@@ -1,5 +1,6 @@
 from copy import deepcopy
 import re
+import warnings
 from lxml.etree import Element
 from lxml import etree
 from zipfile import ZipFile, ZIP_DEFLATED
@@ -136,36 +137,110 @@ class MailMerge(object):
                 fields.add(mf.attrib['name'])
         return fields
 
-    def merge_pages(self, replacements):
+    def merge_templates(self, replacements, separator):
         """
-        Duplicate template page. Creates a copy of the template for each item
-        in the list, does a merge, and separates the them by page breaks.
+        Duplicate template. Creates a copy of the template, does a merge, and separates them by a new paragraph, a new break or a new section break.
+        separator must be :
+        - page_break : Page Break. 
+        - column_break : Column Break. ONLY HAVE EFFECT IF DOCUMENT HAVE COLUMNS
+        - textWrapping_break : Line Break.
+        - continuous_section : Continuous section break. Begins the section on the next paragraph.
+        - evenPage_section : evenPage section break. section begins on the next even-numbered page, leaving the next odd page blank if necessary.
+        - nextColumn_section : nextColumn section break. section begins on the following column on the page. ONLY HAVE EFFECT IF DOCUMENT HAVE COLUMNS
+        - nextPage_section : nextPage section break. section begins on the following page.
+        - oddPage_section : oddPage section break. section begins on the next odd-numbered page, leaving the next even page blank if necessary.
         """
+
+        #TYPE PARAM CONTROL AND SPLIT
+        valid_separators = {'page_break', 'column_break', 'textWrapping_break', 'continuous_section', 'evenPage_section', 'nextColumn_section', 'nextPage_section', 'oddPage_section'}
+        if not separator in valid_separators:
+            raise ValueError("Invalid separator argument")
+        type, sepClass = separator.split("_")
+  
+
+        #GET ROOT - WORK WITH DOCUMENT
         for part in self.parts.values():
             root = part.getroot()
-
             tag = root.tag
             if tag == '{%(w)s}ftr' % NAMESPACES or tag == '{%(w)s}hdr' % NAMESPACES:
                 continue
+		
+            if sepClass == 'section':
 
-            children = []
+                #FINDING FIRST SECTION OF THE DOCUMENT
+                firstSection = root.find("w:body/w:p/w:pPr/w:sectPr", namespaces=NAMESPACES)
+                if firstSection == None:
+                    firstSection = root.find("w:body/w:sectPr", namespaces=NAMESPACES)
+			
+                #MODIFY TYPE ATTRIBUTE OF FIRST SECTION FOR MERGING
+                nextPageSec = deepcopy(firstSection)
+                for child in nextPageSec:
+                #Delete old type if exist
+                    if child.tag == '{%(w)s}type' % NAMESPACES:
+                        nextPageSec.remove(child)
+                #Create new type (def parameter)
+                newType = etree.SubElement(nextPageSec, '{%(w)s}type'  % NAMESPACES)
+                newType.set('{%(w)s}val'  % NAMESPACES, type)
+
+                #REPLACING FIRST SECTION
+                secRoot = firstSection.getparent()
+                secRoot.replace(firstSection, nextPageSec)
+
+            #FINDING LAST SECTION OF THE DOCUMENT
+            lastSection = root.find("w:body/w:sectPr", namespaces=NAMESPACES)
+
+            #SAVING LAST SECTION
+            mainSection = deepcopy(lastSection)
+            lsecRoot = lastSection.getparent()
+            lsecRoot.remove(lastSection)
+
+            #COPY CHILDREN ELEMENTS OF BODY IN A LIST
+            childrenList = root.findall('w:body/*', namespaces=NAMESPACES)
+
+            #DELETE ALL CHILDREN OF BODY
             for child in root:
-                root.remove(child)
-                children.append(child)
+                if child.tag == '{%(w)s}body' % NAMESPACES:
+                    child.clear()
 
+            #REFILL BODY AND MERGE DOCS - ADD LAST SECTION ENCAPSULATED OR NOT
+            lr = len(replacements)
+            lc = len(childrenList)
+            parts = []
             for i, repl in enumerate(replacements):
-                # Add page break in between replacements
-                if i > 0:
-                    pagebreak = Element('{%(w)s}br' % NAMESPACES)
-                    pagebreak.attrib['{%(w)s}type' % NAMESPACES] = 'page'
-                    root.append(pagebreak)
+                for (j, n) in enumerate(childrenList):
+                    element = deepcopy(n)
+                    for child in root:
+                        if child.tag == '{%(w)s}body' % NAMESPACES:
+                            child.append(element)
+                            parts.append(element)
+                            if (j + 1) == lc:
+                                if (i + 1) == lr:
+                                    child.append(mainSection)
+                                    parts.append(mainSection)
+                                else:
+                                    if sepClass == 'section':
+                                        intSection = deepcopy(mainSection)
+                                        p   = etree.SubElement(child, '{%(w)s}p'  % NAMESPACES)
+                                        pPr = etree.SubElement(p, '{%(w)s}pPr'  % NAMESPACES)
+                                        pPr.append(intSection)
+                                        parts.append(p)
+                                    elif sepClass == 'break':
+                                        pb   = etree.SubElement(child, '{%(w)s}p'  % NAMESPACES)
+                                        r = etree.SubElement(pb, '{%(w)s}r'  % NAMESPACES)
+                                        nbreak = Element('{%(w)s}br' % NAMESPACES)
+                                        nbreak.attrib['{%(w)s}type' % NAMESPACES] = type
+                                        r.append(nbreak)
 
-                parts = []
-                for child in children:
-                    child_copy = deepcopy(child)
-                    root.append(child_copy)
-                    parts.append(child_copy)
-                self.merge(parts, **repl)
+                    self.merge(parts, **repl)
+
+    def merge_pages(self, replacements):
+         """
+         Deprecated method.
+         """
+         warnings.warn("merge_pages has been deprecated in favour of merge_templates",
+                      category=DeprecationWarning,
+                      stacklevel=2)         
+         self.merge_templates(replacements, "page_break")
 
     def merge(self, parts=None, **replacements):
         if not parts:
